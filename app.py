ffea0b2d1965f3203c58eb8a0f1c8af509568bb7
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-01-24 09:55:28 -0200

    Temp change in getCurrentMatch() method

--- app.py
+++ app.py
@@ -68,8 +68,9 @@ def getGameVersion():
         hiRezServerStatus = paladinsAPI.getHiRezServerStatus()
         hiRezServerStatus = hiRezServerStatus[1] if platform.startswith("xb") or platform.startswith("sw") else hiRezServerStatus[2] if platform.startswith("ps") else hiRezServerStatus[0]
         patchInfo = paladinsAPI.getPatchInfo()
-    except:
-        return UNABLE_TO_CONNECT_STRINGS[language]
+    except Exception exc:
+        return "{0} : {1} : {2} : {3}".format(type(exc), exc.args, exc, str(exc))
+        #return UNABLE_TO_CONNECT_STRINGS[language]
     return GAME_VERSION_STRINGS[language].format("Paladins", "PC" if platform == "pc" else "PS4" if platform.startswith("ps") else "Nintendo Switch" if platform .startswith("sw") else "Xbox One",
                         PALADINS_UP_STRINGS[language] if hiRezServerStatus.status else PALADINS_DOWN_STRINGS[language],
                         patchInfo.gameVersion, hiRezServerStatus.version)
@@ -149,7 +150,12 @@ def getCurrentMatch():
         if players:
             for play in players:
                 if playerStatusRequest.currentMatchQueueId == 428 or playerStatusRequest.currentMatchQueueId == 486:
-                    rank = PLAYER_RANK_STRINGS[language][play.tier]# if play.tier != 0 else UNRANKED_STRINGS[language] if play.wins + play.losses == 0 else QUALIFYING_STRINGS[language]
+                    #rank = PLAYER_RANK_STRINGS[language][play.tier]# if play.tier != 0 else UNRANKED_STRINGS[language] if play.wins + play.losses == 0 else QUALIFYING_STRINGS[language]
+                    if play.playerName.lower() == player.lower():
+                        rank = PLAYER_RANK_STRINGS[language][getPlayerRequest.rankedController.value if playerStatusRequest.currentMatchQueueId == 428 else getPlayerRequest.rankedKeyboard.value]
+                    else:
+                        getPlayer = paladinsAPI.getPlayer(play.playerId)
+                        rank = PLAYER_RANK_STRINGS[language][getPlayer.rankedController.value if playerStatusRequest.currentMatchQueueId == 428 else getPlayer.rankedKeyboard.value]
                 else:
                     if play.playerName.lower() == player.lower():
                         rank = PLAYER_RANK_STRINGS[language][getPlayerRequest.rankedKeyboard.value if getPlayerRequest.rankedController.wins + getPlayerRequest.rankedController.losses == 0 else getPlayerRequest.rankedController.value]
