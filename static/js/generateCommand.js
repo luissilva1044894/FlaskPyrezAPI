bf335d6f3fe347196c109f7da59229c7d44e1ea8
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-02-22 20:14:56 -0300

    Some Command Generator improvements (Still Beta)

--- static/js/generateCommand.js
+++ static/js/generateCommand.js
@@ -9,63 +9,106 @@ function checkChampName(championName) {
     return false;
 }
 
-//function checkFields(commandName, playerName, championName) { }
-function generateCommand() {
-    // http://nonsocial.herokuapp.com/api/currentmatch?{IGN}&platform={PLATFORM}&language={LANGUAGE}
-    // http://nonsocial.herokuapp.com/api/decks?{IGN}&champion={CHAMPION}&platform={PLATFORM}&language={LANGUAGE}
-    // http://nonsocial.herokuapp.com/api/winrate?{IGN}&champion={CHAMPION}&platform={PLATFORM}&language={LANGUAGE}
-    // http://nonsocial.herokuapp.com/api/lastmatch?{IGN}&platform={PLATFORM}&language={LANGUAGE}
-    // http://nonsocial.herokuapp.com/api/rank?{IGN}&platform={PLATFORM}&language={LANGUAGE}
-    // http://nonsocial.herokuapp.com/api/version?platform={PLATFORM}&language={LANGUAGE}
-    // http://nonsocial.herokuapp.com/api/stalk?{IGN}&platform={PLATFORM}&language={LANGUAGE}
-
-    var commandName = getElementById("command_name"),
+function generateCommand() { // !command add duo Estou duo com X e o elo dele é: (_ELO2_)
+    var commandName = String(getElementById("command_name").value).trim().replace(' ', '').replace('!', ''),
             commandType = getElementById("command_type"),
-            cooldown = getElementById("command_cooldown"),
-            playerName = getElementById("player_name"),
-            championName = getElementById("champion_name"),
+            cooldown = String(getElementById("command_cooldown").value).length > 0 && getElementById("command_cooldown").value >= 5 && getElementById("command_cooldown").value <= 300 ? defaultFor(getElementById("command_cooldown").value) : String(commandType.value).toLowerCase() === "currentmatch" ? 25 : 5;
+            playerName = getElementById("player_name"), // Usar o PaladinsGuru para ver se o player existe: https://github.com/Protovision/paladins_scouter/blob/master/paladins_scouter.c
+            championName = checkChampName(defaultFor(getElementById("champion_name").value, "")) ? getElementById("champion_name").value : "",
             platform = getElementById("platform_form"),
             language = getElementById("language_form"), // int
             botName = getElementById("bot_name"), // int
-            userAccess = getElementById("user_access"),
-            resultsElement = getElementById("results");
-    cmd = ""
+            userLevel = getElementById("user_access"),
+            userCanUse = getElementById("user_can_use");
+    var endpointLink = getEndpoint().replace("index.html", "") + "api/" + String(commandType.value);
+    $("#result-warning").show();
+    if (commandName.length > 0 && String(playerName.value).trim().replace(' ', '').length > 3) {
+        var permLvl = "", cmd = "", cmdChat = "";
 
-    var endpointLink = getEndpoint().replace("index.html", "") + "api/" + String(commandType.value);//"{{ url_for('commandType.value', _external=True) }}";
-    // alert(checkChampName(String(championName.value).trim().replace(' ', '').replace("'", "").toLowerCase()))
-    if (String(commandName.value).trim().replace(' ', '').length > 0 && String(playerName.value).trim().replace(' ', '').length > 3) {
-        if(String(botName.value) === "1") {
-            perm = ""
-            switch(userAccess.value) {
-                case "2" : perm = "reg"; break;
-                case "3" : perm = "susbcriber"; break;
-                case "4" : perm = "mod"; break;
-                case "5" : perm = "owner"; break;
-                default: perm = "everyone";
-            }
-            if(String(commandType.value).toLowerCase() === "getdecks" || String(commandType.value).toLowerCase() === "getwinrate") {
-                cmd = "!addcom -cd={cd} -ul={perm} !{commandName} $(eval `$(querystring)`.trim()==''?'$(customapi {endpointLink}?player={playerName}&platform={platform}&champion={championName}&language={language})':'$(customapi {endpointLink}?query=$(querystring)&champion=$(2)&platform=$(3)&language={language})' ; )";
-                cmd = cmd.replace("{endpointLink}", endpointLink).replace("{cd}", cooldown.value).replace("{perm}", perm).replace("{commandName}", commandName.value).replace("{commandType}", commandType.value).replace("{playerName}", encodeURI(playerName.value)).replace("{platform}", platform.value).replace("{language}", language.value).replace("{championName}", championName.value)
-                cmd = cmd.replace("{endpointLink}", endpointLink).replace("{commandType}", commandType.value).replace("{language}", language.value)
-                getElementById("demo").innerHTML = cmd
-                alert(cmd);
-            } else {
-                cmd = "!addcom -cd={cd} -ul={perm} !{commandName} $(eval `$(querystring)`.trim()==''?'$(customapi {endpointLink}?player={playerName}&platform={platform}&language={language})':'$(customapi {endpointLink}?query=$(querystring)&platform=$(2)&language={language})' ; )";
-                cmd = cmd.replace("{endpointLink}", endpointLink).replace("{cd}", cooldown.value).replace("{perm}", perm).replace("{commandName}", commandName.value).replace("{commandType}", commandType.value).replace("{playerName}", encodeURI(playerName.value)).replace("{platform}", platform.value).replace("{language}", language.value)
-                cmd = cmd.replace("{endpointLink}", endpointLink).replace("{commandType}", commandType.value).replace("{language}", language.value)
-                getElementById("demo").innerHTML = cmd
-                alert(cmd);
-            }
+        switch(botName.value) {
+            case "2" :
+                switch(userLevel.value) {
+                    case "2" : default: permLvl = "%1"; break;
+                    case "3" : permLvl = "%2"; break;
+                    case "4" :  case "5" : "%8"; break;
+                }
+                cmdChat += "!addcom !{CMD_NAME} {PERM_LVL} ".replace("{CMD_NAME}", commandName).replace("{PERM_LVL}", permLvl);
+                cmd += userCanUse.checked ? "@customapi@[{ENDPOINT_LINK}?player=@target@[1]&platform=@target@[3]&champion=@target@[2]&language={LANGUAGE}&channel=@channel@&bot={BOT_NAME}&user=@user@)" : "@customapi@[{ENDPOINT_LINK}?player={PLAYER_NAME}&platform={PLATFORM}&language={LANGUAGE}&channel=@channel@&bot={BOT_NAME}&user=@user@)";
+            break;
+            case "3" : case "5" :
+                switch(userLevel.value) {
+                    case "2" : permLvl = "+r"; break;
+                    case "3" : permLvl = "+s"; break;
+                    case "4" : permLvl = "+m"; break;
+                    case "5" : permLvl = botName.value === "7" ? "+e" : "+c"; break;
+                    default: permLvl = "+a";
+                }
+                cmdChat += "!command add !{CMD_NAME} {PERM_LVL} ".replace("{CMD_NAME}", commandName).replace("{PERM_LVL}", permLvl);
+                cmd += userCanUse.checked ? "$readapi({ENDPOINT_LINK}?query=$dummyormsg&platform={platform}&language={LANGUAGE}&channel=$mychannel&user=$realuser&bot={BOT_NAME})" : "$readapi({ENDPOINT_LINK}?player={PLAYER_NAME}&platform={PLATFORM}&language={LANGUAGE}&channel=$mychannel&user=$realuser&bot={BOT_NAME})";
+            break;
+            case "4" :
+                switch(userLevel.value) {
+                    case "2" : permLvl = 300; break;
+                    case "3" : permLvl = 250; break;
+                    case "4" : permLvl = 500; break;
+                    case "5" : permLvl = 2000; break;
+                    case "6" : permLvl = 1000; break;
+                    default: permLvl = 100;
+                }
+                cmdChat += "!command add !{CMD_NAME} ".replace("{CMD_NAME}", commandName).replace("{PERM_LVL}", permLvl);
+                if(String(commandType.value).toLowerCase() === commandType["1"].text.toLowerCase() || String(commandType.value).toLowerCase() === commandType["2"].text.toLowerCase()) {
+                    cmd += userCanUse.checked ? "${customapi.{ENDPOINT_LINK}?query=${1:}&champion=${2:}&platform=${3:}&language={LANGUAGE}&channel=${channel}&bot={BOT_NAME}&user=${sender}}" : "${customapi.{ENDPOINT_LINK}?player={PLAYER_NAME}&platform={PLATFORM}&language={LANGUAGE}&channel=${channel}&bot={BOT_NAME}&user=${sender}}";
+                } else {
+                    if(String(commandType.value).toLowerCase() === commandType["5"].text.toLowerCase()) {
+                        cmd += userCanUse.checked ? "${customapi.{ENDPOINT_LINK}?platform=${1}&language={LANGUAGE}&channel=${channel}&bot={BOT_NAME}&user=${sender}}" : "${customapi.{ENDPOINT_LINK}?platform={PLATFORM}&language={LANGUAGE}&channel=${channel}&bot={BOT_NAME}&user=${sender}}"
+                    } else {
+                        cmd += userCanUse.checked ? "${customapi.{ENDPOINT_LINK}?query=${1:}&platform=${2:}&language={LANGUAGE}&channel=${channel}&bot={BOT_NAME}&user=${sender}}" : "${customapi.{ENDPOINT_LINK}?player={PLAYER_NAME}&platform={PLATFORM}&language={LANGUAGE}&channel=${channel}&bot={BOT_NAME}&user=${sender}}";
+                    }
+                }
+            break;
+            default:
+                switch(userLevel.value) {
+                    case "2" : permLvl = "reg"; break;
+                    case "3" : permLvl = "susbcriber"; break;
+                    case "4" : permLvl = "mod"; break;
+                    case "5" : permLvl = "owner"; break;
+                    default: permLvl = "everyone";
+                }
+                cmdChat += "!addcom -cd={CD} -ul={PERM_LVL} !{CMD_NAME} ".replace("{CD}", cooldown).replace("{PERM_LVL}", permLvl).replace("{CMD_NAME}", commandName)
+                
+                customAPICode = "$(customapi {ENDPOINT_LINK}?{PARAMS})";
+                cmdUsers = "$(eval `$(querystring)`.trim()==''?\"{IF}\":\"{ELSE}\" ; )"
+                if(String(commandType.value).toLowerCase() === commandType["1"].text.toLowerCase() || String(commandType.value).toLowerCase() === commandType["2"].text.toLowerCase()) {
+                    if(userCanUse.checked) {
+                        cmdUsers = cmdUsers.replace("{IF}", customAPICode.replace("{PARAMS}", "player={PLAYER_NAME}&platform={PLATFORM}&champion={championName}&language={LANGUAGE}&channel=$(channel)&bot={BOT_NAME}&user=$(user)"))
+                        cmdUsers = cmdUsers.replace("{ELSE}", customAPICode.replace("{PARAMS}", "query=$(querystring)&champion=$(2)&platform=$(3)&language={LANGUAGE}&channel=$(channel)&bot={BOT_NAME}&user=$(user)"))
+                    } else customAPICode = customAPICode.replace("{PARAMS}", "player={PLAYER_NAME}&platform={PLATFORM}&champion=$(1)&language={LANGUAGE}&channel=$(channel)&bot={BOT_NAME}&user=$(user)");
+                } else {
+                    if(String(commandType.value).toLowerCase() === commandType["5"].text.toLowerCase()) {
+                        if(userCanUse.checked) customAPICode = customAPICode.replace("{PARAMS}", "platform=$(1)&language={LANGUAGE}&channel=$(channel)&bot={BOT_NAME}&user=$(user)");
+                        else customAPICode = customAPICode.replace("{PARAMS}", "platform={PLATFORM}&language={language}&channel=$(channel)&bot={BOT_NAME}&user=$(user)");
+                    } else {
+                        if(userCanUse.checked) {
+                            cmdUsers = cmdUsers.replace("{IF}", customAPICode.replace("{PARAMS}", "player={PLAYER_NAME}&platform={PLATFORM}&language={LANGUAGE}&channel=$(channel)&bot={BOT_NAME}&user=$(user)"))
+                            cmdUsers = cmdUsers.replace("{ELSE}", customAPICode.replace("{PARAMS}", "query=$(querystring)&platform=$(2)&language={LANGUAGE}&channel=$(channel)&bot={BOT_NAME}&user=$(user)"))
+                        } else customAPICode = customAPICode.replace("{PARAMS}", "player={PLAYER_NAME}&platform={PLATFORM}&language={LANGUAGE}&channel=$(channel)&bot={BOT_NAME}&user=$(user)");
+                    }
+                }
+                cmd += userCanUse.checked ? cmdUsers : customAPICode;
+            break;
+        }
+        addAlert("Command !{NAME} <strong>created</strong> successfully!".replace("{NAME}", commandName), "alert-success", true, true, "#result-warning", 1 * 60);
+        cmd = cmd.replace("{ENDPOINT_LINK}", endpointLink).replace("{PLAYER_NAME}", encodeURI(playerName.value)).replace("{PLATFORM}", platform.value).replace("{LANGUAGE}", language.value).replace("{BOT_NAME}", botName[botName.value - 1].text.replace(" ", "")).replace("{championName}", championName)
+        cmd = cmd.replace("{ENDPOINT_LINK}", endpointLink).replace("{LANGUAGE}", language.value).replace("{BOT_NAME}", botName[botName.value - 1].text.replace(" ", ""))
+        addCommandOutput(cmdChat + cmd, cmd, botName[botName.value - 1].text)
+    } else {
+        if(commandName.length <= 0) {
+            addAlert("<strong>Invalid command name</strong>!", "alert-danger", true, true, "#result-warning");
+            $("#command_name").focus();
+        } else {
+            addAlert("<strong>Invalid Player Name</strong>!", "alert-danger", true, true, "#result-warning");
+            $("#result").html('');
+            $("#player_name").focus();
         }
     }
-    // Enable: resultsElement.attributes.removeNamedItem("hidden");
-    
-    // Disable:
-    //var typ = document.createAttribute("hidden");
-    //resultsElement.attributes.setNamedItem(typ)
-
-    // alert(encodeURI(botName.value))
-
-    // alert("?!")
-    // alert("" + commandName.value)
 }
--- static/js/indexMethods.js
+++ static/js/indexMethods.js
@@ -2,15 +2,62 @@ function getElementById(elementName) { return document.getElementById(elementNam
 
 function commandTypeChanged() {
     var commandType = String(getElementById("command_type").value).toLowerCase();
-    getElementById("champion_name").disabled = !(commandType === "getdecks" || commandType === "getwinrate");
+    getElementById("champion_name").disabled = !(commandType === commandType["1"].text.toLowerCase() || commandType === commandType["2"].text.toLowerCase());
 }
 
 function checkButtonPos() {
-    // getElementById("generate_command").disabled = !(getElementById("command_name").value && getElementById("player_name").value)
+    getElementById("generate_command").disabled = !(getElementById("command_name").value && getElementById("player_name").value)
+}
+function onPageLoaded() {
+    getElementById("champion_name").disabled = true;
+    $("#result").html('');
+
+    $("#player_name").on('keypress', function(e) {
+        if(e.keyCode == 13) { $("#generate_command").trigger('click'); }
+    });
+}
+function clearField(divName) {
+    div = defaultFor(divName, "#alerts"), alert_div = $(div);
+    alert_div.html('');
 }
 
-function onPageLoaded() {
-    // getElementById("demo").innerHTML = "Iframe is loaded.";
-    getElementById("champion_name").disabled = true;
-    // getElementById("generate_command").disabled = true;
+function defaultFor(arg, val) { return typeof arg !== 'undefined' ? arg : val; }
+
+function addCommandOutput(codeMsgChat, codeMsgBackend, botName) {
+    div = defaultFor(div, "#result-warning"), alert_div = $(div), divMsg = "";
+
+    divMsg = "<div class=\"alert alert-dismissible alert-success\" role=\"alert\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\"><span aria-hidden=\"true\">&times;</span><span class=\"sr-only\">Close</span></button>";
+    divMsg += "<div id=\"chat-title\" class=\"command-title\"><h4>Copy paste the following command in your chat <small>(YOU DON'T NEED TO CHANGE ANYTHING)</small>: <code id=\"code-chat-bot\">"
+    divMsg += codeMsgChat;
+    divMsg += "</code></h4></div>";
+    divMsg += "<div id=\"backend-title\" class=\"command-title\"><h4>Or put the following command in <span id=\"backend-title-bot\">";
+    divMsg += botName;
+    divMsg += "</span> backend <small>(YOU DON'T NEED TO CHANGE ANYTHING)</small>: <code id=\"code-backend-bot\">";
+    divMsg += codeMsgBackend;
+    divMsg += "</code></h4></div>";
+    alert_div.append(divMsg);
+}
+
+function addAlert(message, classes, clear, dismiss, div, timer = 1 * 60) {
+    var dismiss = defaultFor(dismiss, true), div = defaultFor(div, "#result-warning"), alert = "", alert_div = $(div);
+
+    if(clear) clearField(div);
+
+    classe = (classes == 'alert-locked' || classes == 'alert-unlocked') ? 'alert-info' : classes;
+    alert = '<div class="alert alert-dismissible ' + classe + '" role="alert">';
+    if(dismiss)
+        alert += '<button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>';
+
+    switch(classes) {
+        case 'alert-danger' : alert += '<span class="fa fa-exclamation-circle" aria-hidden="true"></span> '; break;
+        case 'alert-success' : alert += '<span class="fa fa-check" aria-hidden="true"></span> '; break;
+        case 'alert-warning' : alert += '<span class="fa fa-flag" aria-hidden="true"></span> '; break;
+        case 'alert-info' : alert += '<span class="fa fa-cog" aria-hidden="true"></span> '; break;
+        case 'alert-locked' : alert += '<span class="fa fa-lock" aria-hidden="true"></span> '; break;
+        case 'alert-unlocked' : alert += '<span class="fa fa-unlock" aria-hidden="true"></span> '; break;
+    }
+    alert += message + '</div>';
+    //if(timer != 0) var timeout = setInterval(function() { if(timer == 0) { clearInterval(timeout); alert_div.hide(); } else { console.log(--timer); } }, 1000);
+
+    alert_div.append(alert);
 }
