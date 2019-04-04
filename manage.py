8c2f3b1979d79105aaa3b6882bec23a5aba3e258
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-04-04 13:11:00 -0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-from flask_script import Manager
+from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 
 from app import app, db
@@ -7,6 +7,7 @@ migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command("db", MigrateCommand)
+manager.add_command("debug", Server(host="0.0.0.0", port=8080, use_debugger=True))
 
 if __name__ == "__main__":
     db.create_all()
