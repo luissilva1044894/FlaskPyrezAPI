0cdfde91bf327379742131b67e98d6f5b8647184
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-04-04 13:47:42 -0300

    Added `scripts` - postdeploy

--- manage.py
+++ manage.py
@@ -10,5 +10,5 @@ manager.add_command("db", MigrateCommand)
 manager.add_command("debug", Server(host="0.0.0.0", port=8080, use_debugger=True))
 
 if __name__ == "__main__":
-    db.create_all()
+    #db.create_all()
     manager.run()
