92206df24f05b1d89bd30db3f1681cfdfdf79b9e
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-04-09 14:22:40 -0300

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -80,7 +80,6 @@ function onPageLoaded() {
         //keyCodeTrigger(e.keyCode);
         var commandCooldown = getElementById("command_cooldown");
         commandCooldown.value = commandCooldown.value % commandCooldown.max;
-        console.log(commandCooldown.value);
     });
     $("#player_name").on("keypress", function(e) {
         //checkButtonPos();
@@ -99,13 +98,12 @@ function clearField(divName) {
 function defaultFor(arg, val) { return typeof arg !== 'undefined' ? arg : val; }
 
 function addCommandOutput(codeMsgChat, codeMsgBackend, botName, lang="en") {
-    div = defaultFor(div, "#result-warning"), alert_div = $(div), divMsg = "";
-    if (botName === "Nightbot") {
-        botName = "<a href=\"https://beta.nightbot.tv/commands/custom\" target=\"blank\" title=\"Nightbot Dashboard\">Nightbot</a>"
-    } else if (botName === "Stream Elements") {
-        botName = "<a href=\"https://streamelements.com/dashboard/bot/commands/custom\" target=\"blank\" title=\"Stream Elements Dashboard\">Stream Elements</a>"
-    } else if (botName === "Botisimo") {
-        botName = "<a href=\"https://botisimo.com/account/commands\" target=\"blank\" title=\"Botisimo Dashboard\">Botisimo</a>"
+    div = defaultFor(div, "#result-warning"), alert_div = $(div), divMsg = "", botElement = getElementById("bot_name");
+
+    switch(userLevel.value.toLowerCase()) {
+        case botElement["1"].value.toLowerCase(): botName = "<a href=\"https://botisimo.com/account/commands\" target=\"blank\" title=\"Botisimo Dashboard\">Botisimo</a>"; break;
+        case botElement["2"].value.toLowerCase(): botName = "<a href=\"https://beta.nightbot.tv/commands/custom\" target=\"blank\" title=\"Nightbot Dashboard\">Nightbot</a>"; break;
+        case botElement["3"].value.toLowerCase(): botName = "<a href=\"https://streamelements.com/dashboard/bot/commands/custom\" target=\"blank\" title=\"Stream Elements Dashboard\">Stream Elements</a>"; break;
     }
 
     divMsg = "<div class=\"alert alert-dismissible alert-success\" role=\"alert\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\"><span aria-hidden=\"true\">&times;</span><span class=\"sr-only\">{CLOSE}</span></button>".replace("{CLOSE}", getTranslatedString(lang, "close"));
