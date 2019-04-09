af9b82d0e9e1a0bb3f5541502fa0166156a97edb
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-04-09 15:02:07 -0300

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -100,16 +100,16 @@ function defaultFor(arg, val) { return typeof arg !== 'undefined' ? arg : val; }
 function addCommandOutput(codeMsgChat, codeMsgBackend, botName, lang="en") {
     div = defaultFor(div, "#result-warning"), alert_div = $(div), divMsg = "", botElement = getElementById("bot_name");
 
-    if (botName.toLowerCase() === botElement["0"].value.toLowerCase())
+    if (botName.toLowerCase() === botElement["0"].text.toLowerCase())
         botName = "<a href=\"https://botisimo.com/account/commands\" target=\"blank\" title=\"Botisimo Dashboard\">Botisimo</a>";
-    else if (botName.toLowerCase() === botElement["1"].value.toLowerCase())
+    else if (botName.toLowerCase() === botElement["1"].text.toLowerCase())
         botName = "<a href=\"https://beta.nightbot.tv/commands/custom\" target=\"blank\" title=\"Nightbot Dashboard\">Nightbot</a>";
-    else if (botName.toLowerCase() === botElement["2"].value.toLowerCase())
+    else if (botName.toLowerCase() === botElement["2"].text.toLowerCase())
         botName = "<a href=\"https://streamelements.com/dashboard/bot/commands/custom\" target=\"blank\" title=\"Stream Elements Dashboard\">Stream Elements</a>";
     console.log(botName.toLowerCase())
     console.log(botElement.value.toLowerCase())
-    console.log(botElement["0"].value.toLowerCase())
-    console.log(botElement[botElement.value - 1].value.toLowerCase())
+    console.log(botElement["0"].text.toLowerCase())
+    console.log(botElement[botElement.value - 1].text.toLowerCase())
 
 
     divMsg = "<div class=\"alert alert-dismissible alert-success\" role=\"alert\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\"><span aria-hidden=\"true\">&times;</span><span class=\"sr-only\">{CLOSE}</span></button>".replace("{CLOSE}", getTranslatedString(lang, "close"));
