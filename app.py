249d5e25a0cb0180b3957dcb69588ec0efa49d93
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-04-23 18:15:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -304,6 +304,7 @@ def getRank():
                                 "" if r1.currentRank == Tier.Unranked and r1.wins + r1.losses == 0 else WINS_LOSSES_STRINGS[language].format(formatDecimal(r1.wins), formatDecimal(r1.losses)),
                                 " (Win rate Global: {0}%{1})".format(getPlayerRequest.getWinratio(), "" if r1.wins + r1.losses == 0 else " & Ranked: {0}%".format(r1.getWinratio())))
 def checkChampName(championName):
+    championName = "bombking" if "bk" in championName or "bomb" in championName else "maldamba" if "mal" in championName else championName
     champs = [ "androxus", "atlas", "ash", "barik", "bombking", "buck", "cassie", "dredge", "drogoz", "evie", "fernando", "furia", "grohk", "grover",
     "imani", "inara", "jenos", "khan", "kinessa", "koga", "lex", "lian", "maeve", "makoa", "maldamba", "moji", "pip", "ruckus",
     "seris", "shalin", "skye", "strix", "talus", "terminus", "torvald", "tyra", "viktor", "vivian", "willo", "ying", "zhin" ];
@@ -334,7 +335,7 @@ def getWinrate():
         return INTERNAL_ERROR_500_STRINGS[language]
     if championName:
         if not checkChampName(championName):
-            championName = "bombking" if "bk" in championName or "bomb" in championName else "maldamba" if "mal" in championName else championName
+            return CHAMP_NOT_PLAYED_STRINGS[language].format(playerName, championName)
         for champ in playerGlobalKDA:
             if champ.godName.lower().replace(" ", "").replace("'", "") == championName:
                 return CHAMP_WINRATE_STRINGS[language].format(PLAYER_LEVEL_STRINGS[language].format(champ.godName.replace("'", " "), champ.godLevel), champ.wins, champ.losses,
