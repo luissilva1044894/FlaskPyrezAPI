183aaf447d52a127f22dbc7f4a670f9ae33ca15d
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-05-02 23:45:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -184,9 +184,9 @@ def getPlayerId(playerName, platform = PlatformsSupported.PC):
         _player = Player(name=playerName, id=temp[0].playerId, platform=str(platform))
     return _player.id if _player else -1
 def getLastSeen(lastSeen, language = LanguagesSupported.English):
-    now = datetime.now()
+    now = datetime.utcnow()
     delta = now - lastSeen
-    print("Delta: {}".format(delta))
+    print("Now: {} | Last Seen: {} | Delta: {} | ???: {}".format(now, lastSeen, delta, datetime.now() - lastSeen))
     hours, remainder = divmod(int(delta.total_seconds()), 3600)
     minutes, seconds = divmod(remainder, 60)
     days, hours = divmod(hours, 24)
