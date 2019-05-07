8d32259c33054513542b46080acc00bd1f28f84d
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-05-07 14:24:51 -0300

    Update env.py

--- migrations/env.py
+++ migrations/env.py
@@ -22,8 +22,7 @@ logger = logging.getLogger('alembic.env')
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 from flask import current_app
-config.set_main_option('sqlalchemy.url',
-                       current_app.config.get('SQLALCHEMY_DATABASE_URI'))
+config.set_main_option('sqlalchemy.url', current_app.config.get('SQLALCHEMY_DATABASE_URI'))
 target_metadata = current_app.extensions['migrate'].db.metadata
 
 # other values from the config, defined by the needs of env.py,
@@ -45,10 +44,8 @@ def run_migrations_offline():
 
     """
     url = config.get_main_option("sqlalchemy.url")
-    context.configure(
-        url=url, target_metadata=target_metadata, literal_binds=True
-    )
-
+    context.configure(url=url, target_metadata=target_metadata, literal_binds=True)
+    
     with context.begin_transaction():
         context.run_migrations()
 
