5995c8399a280f296707bb63ab0c45587ca93748
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-05-29 13:23:38 -0300

    Update config.py

--- config.py
+++ config.py
@@ -6,16 +6,14 @@ class BaseConfig(object):
     from decouple import config, Csv
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    PYREZ_AUTH_ID = config("PYREZ_AUTH_ID") or os.environ("PYREZ_AUTH_ID")
-    PYREZ_DEV_ID = config("PYREZ_DEV_ID") or os.environ("PYREZ_DEV_ID")
+    PYREZ_AUTH_ID = os.environ("PYREZ_AUTH_ID") or config("PYREZ_AUTH_ID")
+    PYREZ_DEV_ID = os.environ("PYREZ_DEV_ID") or config("PYREZ_DEV_ID")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///{}.db".format(__name__)
 class DevelopementConfig(BaseConfig):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEVELOPMENT_DATABASE_URI') or "sqlite:///{}.db".format(__name__)
  
 class TestingConfig(BaseConfig):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('TESTING_DATABASE_URI') or "sqlite:///{}.db".format(__name__) 
  
 class ProductionConfig(BaseConfig):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "sqlite:///{}.db".format(__name__)
