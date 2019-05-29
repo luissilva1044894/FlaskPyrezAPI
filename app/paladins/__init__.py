6632f0fa981906fff7348c6077919c5e799f6260
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-05-29 17:31:00 -0300

    Create __init__.py

---
+++ app/paladins/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def register_app(app):
+	from .views import paladins
+
+	app.register_blueprint(paladins, url_prefix="/paladins")
+	app.register_blueprint(paladins, url_prefix="/api")
+
+	return app
