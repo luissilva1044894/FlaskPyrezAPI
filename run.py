7845125ff7ed3baf0ee34d6774bbee18ad0afa77
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-05-31 19:18:02 -0300

    Update run.py

--- run.py
+++ run.py
@@ -5,3 +5,5 @@ from app import create_app
 if __name__ == "__main__":
     app = create_app()
     app.run()
+#https://gist.github.com/rochacbruno/b1fe0ccab1a81804def887e8ed40da57
+#https://gist.github.com/rochacbruno/e44c1f0f43e89093bf7ddba77ee9feef
