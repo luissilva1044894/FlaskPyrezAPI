8638da5a16652dd2a0d6b1473b5bf6355487a8c2
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-06-03 10:02:02 -0300

    Update views.py

--- app/paladins/views.py
+++ app/paladins/views.py
@@ -5,6 +5,7 @@ from flask import Blueprint, render_template, request
 paladins = Blueprint("paladins", __name__, static_folder="static", template_folder="templates", static_url_path='')
 
 @paladins.route('/', methods=["GET"])
+#@paladins.route('/<>')
 def index():
 	from .. import index
 
