0ff1cb7be3ce1b0bc8d7282ca6c517eeb638d474
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-06-03 14:12:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -150,7 +150,7 @@ def limit_remote_addr():#ip = request.remote_addr
     print("*" * 40)
     print(request.headers.keys)
     print("*" * 40)
-    if request.headers.keys["User-Agent"].rfind("Nimbostratus-Bot"):
+    if request.headers["User-Agent"].rfind("Nimbostratus-Bot"): #request.headers.get("User-Agent")
         abort(403)
         print("ABORTED")
 
