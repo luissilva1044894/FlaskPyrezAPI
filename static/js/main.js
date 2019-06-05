01b1be6be56b363682b54dbb8e992697293b7836
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-06-05 12:38:46 -0300

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -91,6 +91,11 @@ function onPageLoaded() {
         //checkButtonPos();
         keyCodeTrigger(e.keyCode, "#generate_command");
     });
+    $("#user_access").on("change", function() {
+        var commandType, userCanUse = getElementById("command_type"), getElementById("user_can_use");
+
+        userCanUse.checked = commandType.value.toLowerCase() === commandType["3"].value.toLowerCase() || commandType.value.toLowerCase() === commandType["4"].value.toLowerCase();
+    }
 }
 function clearField(divName) {
     div = defaultFor(divName, "#alerts"), alertDiv = $(div);
