a041cf0dfed1ea218b9af5176a561c1617f2ddd6
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-10-26 20:43:32 -0300

    Update controllers.py

--- app/twitch/controllers.py
+++ app/twitch/controllers.py
@@ -1,18 +1,20 @@
 
 def random_viewer_function(_channel, _exclude, _ignore):
 	from ..utils import get_url
-	_json = get_url('http://tmi.twitch.tv/group/user/{}/chatters'.format(_channel))
+	_json = get_url('http://tmi.twitch.tv/group/user/{}/chatters'.format(str(_channel).lower()))
 	for x in str(_ignore).split(','):
 		if x.lower() == 'bots':
 			_exclude += 'nightbot,priestbot,streamelements,streamlabs,botisimo,moobot'.split(',')
 			_exclude += 'twitchprimereminder,commanderroot,anotherttvviewer,electricallongboard'.split(',')
+		elif x.lower() == 'mods':
+			x = 'moderators'
 		try:
 			_json['chatters'].pop(x)
 		except (KeyError, TypeError): #ValueError = _json['bots']
 			pass
 	_final_list = []
 	if len(_json) < 1:
-		return "Error: No chatters"
+		return "The list of users is empty"
 	for x in _json['chatters']:
 		for y in _json['chatters'][x]:
 			for z in _exclude:
