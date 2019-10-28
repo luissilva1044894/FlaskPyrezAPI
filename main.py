efcba85ab8e11386ba2da80bcdc1643869aad168
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-10-27 21:57:38 -0300

    Remove release command

--- main.py
+++ main.py
@@ -461,3 +461,4 @@ if __name__ == '__main__':
     #port = int(os.getenv('PORT', 5000))
     #print('Starting app on port %d' % port)
     #app.run(debug=DEBUG, port=port, host='0.0.0.0')
+#release: python manage.py db migrate
