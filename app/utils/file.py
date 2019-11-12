b79a513eaea81101ce33af4622d4388af64dabeb
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-11-12 01:02:30 -0300

    Update file.py

--- app/utils/file.py
+++ app/utils/file.py
@@ -4,11 +4,11 @@ def open_if_exists(filename, mode='rb', encoding='utf-8'):
     import os
 
     #os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir))) # allow setup.py to be run from any path
-    print(os.path.join(os.path.abspath(os.path.dirname(__file__)), filename))
     app_dir = os.path.abspath(os.path.dirname(__file__))
+    print(os.path.join(os.path.abspath(os.path.dirname(__file__)).replace('app\\utils', ''), filename))
     print(app_dir.replace('app\\utils', '') + filename)
     print('EXIST: {} '.format(os.path.isfile(filename)))
-    print('EXIST: {} '.format(app_dir.replace('app\\utils', '') + filename))
+    print('EXIST: {} '.format(os.path.isfile(''.join([app_dir.replace('app\\utils', ''), filename]))))
     if not os.path.isfile(''.join([app_dir.replace('app\\utils', ''), filename])):#os.path.isfile(filename):
     	return None
     return open(filename, mode=mode)#, encoding=encoding)
