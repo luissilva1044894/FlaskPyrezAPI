358653d0779bd20351efb0d8c8e076ad199cfcb1
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-11-12 00:46:37 -0300

    Update file.py

--- app/utils/file.py
+++ app/utils/file.py
@@ -2,16 +2,18 @@
 def open_if_exists(filename, mode='rb', encoding='utf-8'):
     """Returns a file descriptor for the filename if that file exists, otherwise ``None``."""
     import os
+    print('EXIST: {} '.format(os.path.isfile(filename)))
     if not os.path.isfile(filename):
-        return None
+    	return None
     return open(filename, mode=mode)#, encoding=encoding)
 
 def read_json(filename, mode='rb', encoding='utf-8'):
 	import json
-	__x = open_if_exists(filename, mode, encoding)
-	print(__x)
-	with __x as f:
-		return json.load(f)
+	_file = open_if_exists(filename, mode, encoding)
+	if _file:
+		with _file as f:
+			return json.load(f)
+	return {}
 	#from json.decoder import JSONDecodeError
 	#try:
 	#except json.decoder.JSONDecodeError:
