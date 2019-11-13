23eb75fea6bd6e9399d6c6d68fe082db2ffa48b6
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-11-13 15:56:15 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,31 +6,6 @@
 #https://flask.palletsprojects.com/en/1.1.x/patterns/favicon/
 
 #https://stackoverflow.com/questions/4239825/static-files-in-flask-robot-txt-sitemap-xml-mod-wsgi
-def create_app():
-	from flask import Flask
-	from flask_sqlalchemy import SQLAlchemy
-	from .utils import get_env
-	import os
-
-	def get_config(x=None):
-		return {
-			'development': 'config.DevelopementConfig',
-            'dev': 'config.DevelopementConfig',
-            'testing': 'config.TestingConfig',
-            'default': 'config.ProductionConfig',
-            'production': 'config.ProductionConfig',
-            'prod': 'config.ProductionConfig'
-        }.get(str(x).lower(), 'config.ProductionConfig')
-	app = Flask(__name__.split('.')[0], static_folder='static', template_folder='templates', static_url_path='', instance_relative_config=True)
-	app.config.from_object(get_config(get_env('FLASK_ENV', default='dev' if os.sys.platform == 'win32' else 'prod')))
-	app.config.from_pyfile('config.cfg', silent=True)
-	print(app.secret_key)
-
-	# Blueprints
-	register(app)
-
-	return app, SQLAlchemy(app)
-
 def register(app):
 	from .views import blueprint
 	from .utils import replace
