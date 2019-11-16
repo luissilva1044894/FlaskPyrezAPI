212e26ec897db876b6b80eafbf5c77c8f4590d02
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-11-16 02:03:43 -0300

    Update bot_worker.py

--- bot_worker.py
+++ bot_worker.py
@@ -1,7 +1,7 @@
 
 if __name__ == '__main__':
 	from discord_bot import Bot
-	from config import DiscordConfig
+	from utils.discord import DiscordConfig
 	from utils import get_env
 	bot = Bot(config=DiscordConfig())
 	bot.run(get_env('BOT_TOKEN', None))
