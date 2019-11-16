b3b995c45d6f74d21873c9f23268e4443fd5ac08
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-11-16 10:40:13 -0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@
 
 from flask_script import Manager, Server
 from web import app
+from utils import get_env
 
 #from flask_sqlalchemy import SQLAlchemy
 #from flask_migrate import Migrate, MigrateCommand
@@ -11,7 +12,13 @@ from web import app
 #app = create_app()
 
 manager = Manager(app)
-manager.add_command('debug', Server(host='127.0.0.1', port=8080, use_debugger=True))
+_debug_mod = get_env('DEBUG', default=not 'heroku' in get_env('PYTHONHOME', '').lower())
+manager.add_command('debug', Server(host=get_env('HOST', default='0.0.0.0'), port=get_env('PORT', default=5000), use_debugger=_debug_mod))
+
+@app.shell_context_processor
+def make_shell_context():
+	return dict(app=app)#, db=db)
+
 #@manager.command
 #def debug():
 #	app.run(host='127.0.0.1', port=8080, use_debugger=True, debug=True)
@@ -34,8 +41,13 @@ if __name__ == '__main__':
 		import os
 		for q in psutil.process_iter():
 			if q.name().startswith('python'):
-				if q.pid != os.getpid() and [_ for _ in q.cmdline() if os.path.basename(__file__) in _]:
+				if not _debug_mod and q.pid != os.getpid() and [_ for _ in q.cmdline() if os.path.basename(__file__) in _]:
 					import sys
 					print(f'App is already running!')#Bot is already running!
 					sys.exit()
+	if _debug_mod:
+		@app.route('/debug/')
+		def _debug():
+			"""deliberate error, test debug working"""
+			assert False, 'oops'
 	manager.run()
