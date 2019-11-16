a5bfddeb22bb15c5634f875bcae2a5b92e595030
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-11-16 08:44:30 -0300

    Update views.py

--- web/api/paladins/views.py
+++ web/api/paladins/views.py
@@ -5,15 +5,33 @@ from flask import Blueprint
 
 blueprint = Blueprint(__name__.split('.', 1)[1], __name__, static_url_path='', url_prefix='/{}'.format(__name__.split('.', 1)[1].replace('.views', '').replace('.', '/')))
 
+if not hasattr(blueprint, 'paladins_api'):
+	from pyrez.api import PaladinsAPI
+	from utils import get_env
+	blueprint.paladins_api = PaladinsAPI(devId=get_env('PYREZ_DEV_ID'), authKey=get_env('PYREZ_AUTH_ID'))
+
+'''
+@blueprint.before_app_first_request
+@blueprint.before_app_request
+@blueprint.before_request
+def before_app_first_request_func():
+	print('before_app_request')
+@blueprint.app_errorhandler(404) #https://stackoverflow.com/questions/12768825/flask-error-handler-for-blueprints
+def app_errorhandler(error=None):
+	from flask import request
+	if request.full_path.rfind(__name__.split('.', 1)[1].replace('.views', '').replace('.', '/')) != -1:
+		return error
+	return '?'
+'''
+
 def get_page():
 	from flask import request
 	return ' '.join([blueprint.name, request.url_rule.rule])
 
-#@blueprint.app_errorhandler(404) #https://stackoverflow.com/questions/12768825/flask-error-handler-for-blueprints
 @blueprint.route('/', methods=['GET'])
-def root_handler(error=None):
+def root_handler():
 	"""Homepage route."""
-	return get_page()
+	return str(blueprint.paladins_api.ping())
 
 @blueprint.route('/deck', methods=['GET'])
 @blueprint.route('/decks', methods=['GET'])
