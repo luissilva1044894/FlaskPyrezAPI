ff2845aa3efea27b16bca4c1c8f3f377bec847b7
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-11-16 01:20:41 -0300

    Update config.py

--- config.py
+++ config.py
@@ -51,19 +51,21 @@ class Config(object):
 	SQLALCHEMY_DATABASE_URI = get_env('DATABASE_URL', default='sqlite:///{}.db'.format('app' or __name__))#'sqlite:///:memory:'
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	#SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-
-	#import os
-	#print(os.environ)
 	_binds = get_env('SQLALCHEMY_BINDS', default=None)
+	SQLALCHEMY_BINDS = {}
 	if _binds:
 		#https://docs.sqlalchemy.org/en/13/core/exceptions.html
 		#https://flask-sqlalchemy.palletsprojects.com/en/2.x/binds/
 		#https://flask-migrate.readthedocs.io/en/latest/
-		SQLALCHEMY_BINDS = {}
 		for _ in _binds.split(','):
 			__ = _.split(':', 1)
 			SQLALCHEMY_BINDS.update({__[0].lower() : __[1] if __[1].rfind('://') != -1 else get_env(__[1])})
-		print(SQLALCHEMY_BINDS)
+	else:
+		import os
+		for _ in os.environ:
+			if 'DB_URL' in _.upper():
+				SQLALCHEMY_BINDS.update({_.split('_', 1)[0] : get_env(_)})
+	print(SQLALCHEMY_BINDS)
 	# SECURITY WARNING: don't run with debug turned on in production!
 	#Default: True if ENV is 'development', or False otherwise.
 	DEBUG = boolify(get_env('DEBUG', default=os.sys.platform == 'win32' or os.name == 'nt'))
