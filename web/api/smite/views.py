e9efccaa2a482800cbcde8fb6583d5b2db4412b0
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-11-16 09:06:57 -0300

    Update views.py

--- web/api/smite/views.py
+++ web/api/smite/views.py
@@ -5,6 +5,11 @@ from flask import Blueprint
 
 blueprint = Blueprint(__name__.split('.', 1)[1], __name__, static_url_path='', url_prefix='/{}'.format(__name__.split('.', 1)[1].replace('.views', '').replace('.', '/')))
 
+if not hasattr(blueprint, 'smite_api'):
+	from pyrez.api import SmiteAPI
+	from utils import get_env
+	blueprint.smite_api = SmiteAPI(devId=get_env('PYREZ_DEV_ID'), authKey=get_env('PYREZ_AUTH_ID'))
+
 def get_page():
 	from flask import request
 	return ' '.join([blueprint.name, request.url_rule.rule])
@@ -12,7 +17,7 @@ def get_page():
 @blueprint.route('/', methods=['GET'])
 def root_handler(error=None):
 	"""Homepage route."""
-	return get_page()
+	return str(blueprint.smite_api.ping())
 
 @blueprint.route('/winrate', methods=['GET'])
 @blueprint.route('/kda', methods=['GET'])
