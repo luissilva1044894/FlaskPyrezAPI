bad4b5e647d4679a8bed3ea8e594778869c21e8f
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-11-18 09:44:56 -0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,7 @@
-#from web import app as application
 from web import create_app
 
 app = create_app()
 app.app_context().push()
 
-#from flask_sqlalchemy import SQLAlchemy
-#db = SQLAlchemy(app)
 if __name__ == '__main__':
 	app.run(debug=app.config['DEBUG'], use_debugger=app.config['DEBUG'], use_reloader=app.config['DEBUG'], port=app.config['PORT'], host=app.config['HOST'])
