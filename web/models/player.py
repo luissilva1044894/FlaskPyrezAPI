abac2ce0002080d1287c2c1096730ffe7f0afc8f
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-11-17 22:40:08 -0300

    Fix models

--- web/models/player.py
+++ web/models/player.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from web.models import db, BaseModel
+from web.models import db
 
-class Player(BaseModel):
-    """Model for player accounts."""
-    __tablename__ = 'players'
+class PaladinsPlayer(db.Model):
+    __tablename__ = 'paladins_players'
+    __bind_key__ = 'paladins'
+
+    _index = db.Column('index', db.Integer, primary_key=True, unique=True, nullable=False, autoincrement=True)
 
     id = db.Column(db.Integer, primary_key=False, nullable=False, autoincrement=False)#unique=True, 
     name = db.Column(db.String(120), nullable=False)
     platform = db.Column(db.String(4), nullable=False)
+    #_game = db.Column(db.String(8), primary_key=False, unique=False, nullable=True, autoincrement=False)
 
     def __init__(self, id, name, platform):
         self.id = id
@@ -17,10 +20,58 @@ class Player(BaseModel):
         self.platform = platform
         self.save()
 
-class PaladinsPlayer(Player):
-    __bind_key__ = 'paladins'
+    def save(self):
+        from sqlalchemy.exc import IntegrityError, InternalError, OperationalError, ProgrammingError
+        try:
+            db.session.add(self)
+            db.session.commit()
+        except (IntegrityError, InternalError, OperationalError, ProgrammingError):
+            db.session.rollback()
+            getattr(self.__class__, 'query').filter_by(id=self.id).first().delete()
+            self.save()
+    def __repr__(self):
+        return '<{} {}>'.format(self.__class__.__name__, self.to_json())
+    def __str__(self):
+        return str(self.to_json())
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    def to_json(self):
+        return {f'{_}: {self.__dict__[_]}' for _ in self.__dict__ if not _.startswith('_')}
 
+class SmitePlayer(db.Model):
+    __bind_key__ = 'smite'
+
+    __tablename__ = 'smite_players'
+
+    _index = db.Column('index', db.Integer, primary_key=True, unique=True, nullable=False, autoincrement=True)
+
+    id = db.Column(db.Integer, primary_key=False, nullable=False, autoincrement=False)#unique=True, 
+    name = db.Column(db.String(120), nullable=False)
+    platform = db.Column(db.String(4), nullable=False)
     #_game = db.Column(db.String(8), primary_key=False, unique=False, nullable=True, autoincrement=False)
 
-class SmitePlayer(Player):
-    __bind_key__ = 'smite'
+    def __init__(self, id, name, platform):
+        self.id = id
+        self.name = name
+        self.platform = platform
+        self.save()
+
+    def save(self):
+        from sqlalchemy.exc import IntegrityError, InternalError, OperationalError, ProgrammingError
+        try:
+            db.session.add(self)
+            db.session.commit()
+        except (IntegrityError, InternalError, OperationalError, ProgrammingError):
+            db.session.rollback()
+            getattr(self.__class__, 'query').filter_by(id=self.id).first().delete()
+            self.save()
+    def __repr__(self):
+        return '<{} {}>'.format(self.__class__.__name__, self.to_json())
+    def __str__(self):
+        return str(self.to_json())
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    def to_json(self):
+        return {f'{_}: {self.__dict__[_]}' for _ in self.__dict__ if not _.startswith('_')}
