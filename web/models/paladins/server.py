90061d0c95389a875270de05f77cda1af7f1bcb3
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-11-21 02:29:40 -0300

    Update server.py

--- web/models/paladins/server.py
+++ web/models/paladins/server.py
@@ -50,6 +50,38 @@ class Server(db.Model):
 			'version': self.api_version,
 			'error_msg': error_msg
 		}
+	@staticmethod
+	def get(_api, force=False):
+		from .platform import Platform
+		from .patch_note import PatchNote
+		error_msg = None
+		_server = Server.query.first()
+		if force or not _server or _server and not _server.updated:
+			try:
+				_server_status, _ping = _api.getServerStatus(), _api.ping()
+				[ _.delete() for _ in Platform.query.all()]
+				for _ in _server_status:
+					Platform(name=_.platform if _.environment.lower() != 'pts' else _.environment, limited_access=_.limitedAccess, online=_.status, version=_.version)
+				from utils import get_url
+				_title = get_url('https://cms.paladins.com/wp-json/api/get-posts/1?&search=update%20notes')[0].get('title')
+				_patch_notes, _patch_note = get_url('https://cms.paladins.com/wp-json/api/get-posts/1?&search={}'.format(_title[:_title.rfind('update') - 1])), []
+				[ _.delete() for _ in PatchNote.query.all()]
+				for i in range(len(_patch_notes)):
+					for _lang in [1, 2, 3, 9, 10, 11, 12, 13]:
+						x = get_url('https://cms.paladins.com/wp-json/api/get-post/{}?&slug={}'.format(_lang, _patch_notes[i].get('slug')))  
+						PatchNote(author=x.get('author'), content=x.get('content'), image_header=x.get('featured_image'), image_thumb=_patch_notes[i].get('featured_image'), timestamp=x.get('timestamp'), title=x.get('title'), lang=_lang)
+			except Exception as exc:
+				print(exc)
+				import urllib3
+				if isinstance(exc.args, tuple):
+					for _ in exc.args:
+						if isinstance(_, urllib3.exceptions.MaxRetryError):
+							error_msg = 'An internal Internet error has occurred: This data may be outdated!'
+			else:
+				print('>>> Server status updated!')
+				return {'server': Server(game=_ping.apiName[:-3].lower(), version=_ping.gamePatch, api_version=_ping.apiVersion), 'error': error_msg}
+		return {'server': _server, 'error': error_msg}
+
 	@property
 	def platform(self):
 		from .platform import Platform
