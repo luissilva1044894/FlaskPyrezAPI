31c64b56d990f2f8452be533fa1f3bbb24040c08
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-11-21 02:32:44 -0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -24,20 +24,20 @@ manager.add_command('debug', Server(host=get_env('HOST', default='0.0.0.0'), por
 
 @manager.command
 def create_db():
-	db.create_all()
+  db.create_all()
 @manager.command
 def drop_db():
-	db.drop_all()
+  db.drop_all()
 @manager.command
 def reset_db():
-	db.drop_all()
-	db.create_all()
+  db.drop_all()
+  db.create_all()
 
 @manager.command
 def schedule_task():
-	t = 'i am a scheduled action, yeah'
-	print(t)
-	app.logger.debug(t)
+  t = 'i am a scheduled action, yeah'
+  print(t)
+  app.logger.debug(t)
 
 @app.shell_context_processor
 def make_shell_context():
@@ -45,33 +45,33 @@ def make_shell_context():
 
 #@manager.command
 #def debug():
-#	app.run(host='127.0.0.1', port=8080, use_debugger=True, debug=True)
+#  app.run(host='127.0.0.1', port=8080, use_debugger=True, debug=True)
 
 #migrate = Migrate(app, app._extensions['db'])
 #manager.add_command('db', MigrateCommand)
 
 #@manager.command
 #def create_db(drop_db=False):
-#	if drop_db:
-#		app._extensions['db'].drop_all()
-#	app._extensions['db'].create_all()
+  #if drop_db:
+    #app._extensions['db'].drop_all()
+    #app._extensions['db'].create_all()
 
 if __name__ == '__main__':
-	try:
-		import psutil
-	except ImportError:
-		pass
-	else:
-		import os
-		for q in psutil.process_iter():
-			if q.name().startswith('python'):
-				if not _debug_mod and q.pid != os.getpid() and [_ for _ in q.cmdline() if os.path.basename(__file__) in _]:
-					import sys
-					print(f'App is already running!')#Bot is already running!
-					sys.exit()
-	if _debug_mod:
-		@app.route('/debug/')
-		def _debug():
-			"""deliberate error, test debug working"""
-			assert False, 'oops'
-	manager.run()
+  try:
+    import psutil
+  except ImportError:
+    pass
+  else:
+    import os
+    for q in psutil.process_iter():
+      if q.name().startswith('python'):
+        if not _debug_mod and q.pid != os.getpid() and [_ for _ in q.cmdline() if os.path.basename(__file__) in _]:
+          import sys
+          print(f'App is already running!')#Bot is already running!
+          sys.exit()
+  if _debug_mod:
+  	@app.route('/debug/')
+  	def _debug():
+      """deliberate error, test debug working"""
+      assert False, 'oops'
+  manager.run()
