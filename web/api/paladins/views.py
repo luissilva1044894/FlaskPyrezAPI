b1e6af1aaff4b64c1de7f152967f058201138ceb
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-11-21 20:36:00 -0300

    Update views.py

--- web/api/paladins/views.py
+++ web/api/paladins/views.py
@@ -5,11 +5,12 @@ from flask import Blueprint
 
 blueprint = Blueprint(__name__.split('.', 1)[1], __name__, static_url_path='', url_prefix='/{}'.format(__name__.split('.', 1)[1].replace('.views', '').replace('.', '/')))
 
-if not hasattr(blueprint, 'paladins_api'):
-	from pyrez.api import PaladinsAPI
+if not hasattr(blueprint, '__api__'):
+	#setattr(self, credential.replace('.', '_'), os.getenv(credential))
+	#getattr(self.__class__, 'query').filter_by(id=self.id).first().delete()
+	import pyrez
 	from utils import get_env
-	blueprint.paladins_api = PaladinsAPI(devId=get_env('PYREZ_DEV_ID'), authKey=get_env('PYREZ_AUTH_ID'))
-
+	blueprint.__api__ = getattr(pyrez, '{}API'.format(__name__.split('.')[-2].capitalize()))(devId=get_env('PYREZ_DEV_ID'), authKey=get_env('PYREZ_AUTH_ID'))
 '''
 @blueprint.before_app_first_request
 @blueprint.before_app_request
@@ -24,9 +25,30 @@ def app_errorhandler(error=None):
 	return '?'
 '''
 from ...models.player import Paladins
-from utils.flask import get_player_id, get
+from utils import flask
+from utils.flask import get_player_id, get, get_lang_id
+import requests
+from flask import g
 
-#from flask import request
+@blueprint.errorhandler(requests.exceptions.ConnectionError)
+def connection_error_handler(error=None):
+	return 'Internal Error!'
+
+@blueprint.errorhandler(flask.exceptions.PlayerRequired)
+def player_required_error_handler(error=None):
+	return f"🚫 ERROR: No player name!."
+
+@blueprint.errorhandler(flask.exceptions.ChampRequired)
+def champ_required_error_handler(error=None):
+	return f"🚫 ERROR: No champ name!."
+
+@blueprint.errorhandler(flask.exceptions.NoDeck)
+def no_deck_error_handler(error=None):
+	return f"🚫 ERROR: “{g.__player__}” doesn't have any “{g.__champ__}” custom loadouts!."
+
+@blueprint.errorhandler(flask.exceptions.NoChamp)
+def no_champ_error_handler(error=None):
+	return f"🚫 ERROR: Invalid champ name “{g.__champ__}”!"
 
 def get_page():
 	from flask import request
@@ -35,43 +57,51 @@ def get_page():
 @blueprint.route('/', methods=['GET'])
 def root_handler():
 	"""Homepage route."""
-	return str(blueprint.paladins_api.ping())
+	return str(blueprint.__api__.ping())
 
-@blueprint.route('/deck', methods=['GET'], strict_slashes=False)
 @blueprint.route('/decks', methods=['GET'], strict_slashes=False)
+@flask.decorators.player_required
+@flask.decorators.champ_required
 def decks_handler():
+	from .controllers.decks import func
+
+	_player_id = get_player_id(player_name=get('player'), _db_model=Paladins, _api=blueprint.__api__, platform=get('platform', 'pc'))
+	__rt = func(get('champ', ''), player_id=_player_id, _api=blueprint.__api__, lang=get('lang', get_lang_id()), nodeck_exc=flask.exceptions.NoDeck, nochamp_exc=flask.exceptions.NoChamp)
+	if isinstance(__rt, dict):
+		from flask import jsonify
+		return jsonify(__rt)
+	return str(__rt)
 	return get_page()
 
-@blueprint.route('/winrate', methods=['GET'], strict_slashes=False)
 @blueprint.route('/kda', methods=['GET'], strict_slashes=False)
+@flask.decorators.player_required
 def kda_handler():
 	return get_page()
 
-@blueprint.route('/lastmatch', methods=['GET'], strict_slashes=False)
 @blueprint.route('/last_match', methods=['GET'], strict_slashes=False)
+@flask.decorators.player_required
 def lastmatch_handler():
 	return get_page()
 
-@blueprint.route('/currentmatch', methods=['GET'], strict_slashes=False)
-@blueprint.route('/current_match', methods=['GET'], strict_slashes=False)
-@blueprint.route('/livematch', methods=['GET'], strict_slashes=False)
 @blueprint.route('/live_match', methods=['GET'], strict_slashes=False)
+@flask.decorators.player_required
 def livematch_handler():
 	return get_page()
 
 @blueprint.route('/rank', methods=['GET'], strict_slashes=False)
+@flask.decorators.player_required
 def rank_handler():
 	#from flask import url_for
 	#print(url_for('api.paladins.views.rank_handler', external=True)) > /api/paladins/rank/?external=True
 	#print(url_for('api.paladins.views.rank_handler', external=True, _external=True))
-	return str(get_player_id(player_name=get('player'), _db_model=Paladins, _api=blueprint.paladins_api, platform=get('platform', 'pc')))
+	return str(get_player_id(player_name=get('player'), _db_model=Paladins, _api=blueprint.__api__, platform=get('platform', 'pc')))
 @blueprint.route('/stalk', methods=['GET'], strict_slashes=False)
+@flask.decorators.player_required
 def stalk_handler():
 	return get_page()
 
 @blueprint.route('/version', methods=['GET'], strict_slashes=False)
 def version_handler():
 	from .controllers.version import func
-	from utils.flask import get_lang_id
-	return func(as_json='json' in get('format', '') or get('json'), _api=blueprint.paladins_api, lang=get_lang_id())
+	return func(as_json='json' in get('format', '') or get('json'), _api=blueprint.__api__, lang=get('lang', get_lang_id()))
 	#return get_page()
