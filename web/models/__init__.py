936e216f1099491880ecfe3514e4c64a5a1b6315
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-11-22 09:09:12 -0300

    Update __init__.py

--- web/models/__init__.py
+++ web/models/__init__.py
@@ -7,17 +7,22 @@ db = SQLAlchemy()
 class CRUD_Mixin(object):
   #__bind_key__ = 'database'
   #__abstract__ = True
-  id = db.Column(db.Integer, primary_key=True, autoincrement=False, nullable=False)
+  id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+  my_id = db.Column(db.Integer, nullable=False)
 
-  def add(self, resource=None):
-  	db.session.add(resource or self)
-  	return db.session.commit()
+  def add(self, resource=None, auto_commit=False):
+    db.session.add(resource or self)
+    if auto_commit:
+      return self.commit()
+  def commit(self):
+    return db.session.commit()
   def update(self):
   	return db.session.commit()
-  def delete(self, resource=None):
+  def delete(self, resource=None, auto_commit=False):
     db.session.delete(resource or self)
-    return db.session.commit()
-  def save(self):
+    if auto_commit:
+      return self.commit()
+  def save(self, auto_commit=True):
     '''
   	from sqlalchemy.exc import IntegrityError, InternalError, OperationalError, ProgrammingError
   	try:
@@ -27,11 +32,11 @@ class CRUD_Mixin(object):
   		getattr(self.__class__, 'query').filter_by(id=self.id).first().delete()
   		self.save()
     '''
-    self.add()
+    self.add(auto_commit)
   def __repr__(self):
   	return '<{} {}>'.format(self.__class__.__name__, self.to_json())
   def __str__(self):
   	return str(self.to_json())
   def to_json(self):
   	#return {c.name: getattr(self, c.name) for c in self.__table__.columns}
-  	return {_: self.__dict__[_] for _ in self.__dict__ if not _.startswith('_')}
+  	return {_ if not _ == 'my_id' else 'id': self.__dict__[_] for _ in self.__dict__ if not _.startswith('_') or not _ == 'id'}
