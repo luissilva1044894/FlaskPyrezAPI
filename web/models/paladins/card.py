8186a7b5bc42691d231718b505bc6994df2f50ad
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-11-22 09:09:31 -0300

    Update card.py

--- web/models/paladins/card.py
+++ web/models/paladins/card.py
@@ -5,7 +5,7 @@ class Card(db.Model, CRUD_Mixin):
   __tablename__ = __name__.split('.', 2)[-1].replace('.', '_')
   __bind_key__ = __name__.split('.')[-2]
 
-  id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+  #id = db.Column(db.Integer, primary_key=True, autoincrement=True)
   item_id = db.Column(db.Integer)
   icon_id = db.Column(db.Integer)
   card_id = db.Column(db.Integer)
@@ -23,37 +23,18 @@ class Card(db.Model, CRUD_Mixin):
   #champ_id = db.Column(db.Integer, db.ForeignKey(f'{__bind_key__}_champ.champ_id'))
   champ_id = db.Column(db.Integer)
 
-  def __init__(self, id=0, icon_id=0, card_id=0, name=None, name_english=None, description=None, short_desc=None, actv_schedule=False, lti=False, cooldown=0, is_talent=False, lang=1, champ_id=None):
+  def __init__(self, id=0, icon_id=0, card_id=0, name=None, name_english=None, description=None, short_desc=None, actv_schedule=False, lti=False, cooldown=0, is_talent=False, lang=1, champ_id=None, auto_commit=True):
     from boolify import boolify
+    from utils.paladins import extract_description
     self.item_id = int(id)
     self.icon_id = int(icon_id)
     self.card_id = int(card_id)
     self.name = name
     self.name_english = name_english
-    self.scale = 0
-    import re
-    scale = re.search('=(.+?)\|', description)
-    try:
-      self.scale = float(str(scale.group(1)).replace(',', '.'))
-      # if scale % 2 == 0 or scale % 2 == scale: scale = int(scale)
-    except AttributeError:
-      pass
-    try:
-      description = description.replace('{' + str(re.search('{(.*?)}', description).group(1)) + '}', '{SCALE}')
-    except AttributeError:
-      pass
-    #desc = re.sub('[\[].*?[\]] ', '', desc)
-    self.ability = None
-    match = re.compile(r'\[(.+?)\] (.*)').match(description)
-    if match:
-      self.ability = match.group(1)
-      description = match.group(2).strip()
+    self.scale, description = extract_scale(description)
     self.is_talent = boolify(is_talent)
-    self.description = description
-    match = re.compile(r'\[(.+?)\] (.*)').match(short_desc)
-    if match:
-      short_desc = match.group(2).strip()
-    self.short_description = short_desc
+    self.description, self.ability = extract_description(description)
+    self.short_description, _ = extract_description(short_desc)
     self.activation_schedule = boolify(actv_schedule)
     self.lti = boolify(lti)
     self.cooldown = int(cooldown)
@@ -61,4 +42,4 @@ class Card(db.Model, CRUD_Mixin):
     self.champ_id = -1
     if champ_id:
       self.champ_id = int(champ_id)
-    self.save()
+    self.save(auto_commit)
