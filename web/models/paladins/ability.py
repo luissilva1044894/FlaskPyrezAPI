4eec1c20ef03d71d8c1979244f1980567d79858f
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-11-22 09:09:23 -0300

    Update ability.py

--- web/models/paladins/ability.py
+++ web/models/paladins/ability.py
@@ -5,7 +5,7 @@ class Ability(db.Model, CRUD_Mixin):
 	__tablename__ = __name__.split('.', 2)[-1].replace('.', '_')
 	__bind_key__ = __name__.split('.')[-2]
 
-	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	#id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 	ability_id = db.Column(db.Integer)
 	#damage_type = db.Column(db.Integer)
 	ability = db.Column(db.Text)
@@ -17,22 +17,17 @@ class Ability(db.Model, CRUD_Mixin):
 	#champ_id = db.Column(db.Integer, db.ForeignKey(f'{__bind_key__}_champ.champ_id'))
 	champ_id = db.Column(db.Integer)
 
-	def __init__(self, ability_id, damage_type=0, cooldown=0, description=None, summary=None, lang=1, champ_id=None):
-		from utils.paladins import get_dmg_type
-		import re
+	def __init__(self, ability_id, damage_type=0, cooldown=0, description=None, summary=None, lang=1, champ_id=None, auto_commit=True):
+		from utils.paladins import get_dmg_type, extract_description
 		self.ability_id = int(ability_id)
 		self.damage_type = get_dmg_type(damage_type, True)
 		self.cooldown = int(cooldown)
-		self.ability = None
-		match = re.compile(r'\[(.+?)\] (.*)').match(description)
-		if match:
-			self.ability = match.group(1)
-			description = match.group(2).strip()
+		self.description, self.ability = extract_description(description)
 		self.description = description
 		self.summary = summary
 		self.__lang__ = int(lang)
 		self.champ_id = -1
 		if champ_id:
 			self.champ_id = int(champ_id)
-		self.save()
+		self.save(auto_commit)
 	
