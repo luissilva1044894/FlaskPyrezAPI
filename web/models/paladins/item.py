9d45a32796d64d098f38501c15ad737c4251037e
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-11-22 09:09:39 -0300

    Update item.py

--- web/models/paladins/item.py
+++ web/models/paladins/item.py
@@ -5,7 +5,7 @@ class Item(db.Model, CRUD_Mixin):
   __tablename__ = __name__.split('.', 2)[-1].replace('.', '_')
   __bind_key__ = __name__.split('.')[-2]
 
-  id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+  #id = db.Column(db.Integer, primary_key=True, autoincrement=True)
   item_id = db.Column(db.Integer)
   icon_id = db.Column(db.Integer)
   price = db.Column(db.Integer)
@@ -15,11 +15,12 @@ class Item(db.Model, CRUD_Mixin):
   description = db.Column(db.Text)
   __lang__ = db.Column(db.Integer)
 
-  def __init__(self, id=0, icon_id=0, price=0, item_type=0, name=None, name_english=None, description=None, lang=1):
+  def __init__(self, id=0, icon_id=0, price=0, item_type=0, name=None, name_english=None, description=None, lang=1, auto_commit=True):
     from boolify import boolify
     self.item_id = int(id)
     self.icon_id = int(icon_id)
     self.price = int(price)
+    self.item_type = -1
     if item_type:
       from utils.paladins import get_item_type
       self.item_type = get_item_type(item_type.split(' ')[-2], True)
@@ -27,4 +28,4 @@ class Item(db.Model, CRUD_Mixin):
     #self.name_english = name_english
     self.description = description.split(']', 1)[-1][1:]
     self.__lang__ = lang
-    self.save()
+    self.save(auto_commit)
