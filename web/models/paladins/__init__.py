0e42d80e2b249eb7444918ac32f85ad3b5e8a20f
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-11-21 22:25:43 -0300

    Update __init__.py

--- web/models/paladins/__init__.py
+++ web/models/paladins/__init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from .champ import Champ
-from .item import Item
-from .card import Card
-from .ability import Ability
+#from .champ import Champ
+#from .item import Item
+#from .card import Card
+#from .ability import Ability
 #from .champion_info import ChampionInfo
 #from .ability_info import AbilityInfo
