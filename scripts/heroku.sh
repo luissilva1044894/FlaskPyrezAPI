843c8cd26a53d9072a6a13d483292f59ec11eadf
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-11-23 10:02:53 -0300

    Update heroku.sh

--- scripts/heroku.sh
+++ scripts/heroku.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 if [ -n "$SERVER_MODE" ]; then
-    if [ {"$SERVER_MODE"^^} == *"GUNICORN"* ]; then
-    	echo "Running (async) server as Gunicorn"
-    	gunicorn -b 0.0.0.0:$PORT asgi:app -w 4 -k uvicorn.workers.UvicornWorker
-    elif [ {"$SERVER_MODE"^^} == *"UVICORN"* ]; then
-    	echo "Running (async) server as Uvicorn"
-    	uvicorn asgi:app --host 0.0.0.0 --port $PORT
-    else
-    	echo "Running (async) server as Hypercorn"
-    	hypercorn -b 0.0.0.0:${PORT} asgi:app
-    fi
+	echo "${SERVER_MODE}^^}"
+	echo "${SERVER_MODE}^^}" | awk '{print toupper($0)}'
+	echo "${SERVER_MODE}^^}" | tr '/a-z/' '/A-Z/'
+	if [ ${SERVER_MODE}^^} == *"GUNICORN"* ]; then
+		echo "Running (async) server as Gunicorn"
+		gunicorn -b 0.0.0.0:$PORT asgi:app -w 4 -k uvicorn.workers.UvicornWorker
+	elif [ {"$SERVER_MODE"^^} == *"UVICORN"* ]; then
+		echo "Running (async) server as Uvicorn"
+		uvicorn asgi:app --host 0.0.0.0 --port $PORT
+	else
+		echo "Running (async) server as Hypercorn"
+		hypercorn -b 0.0.0.0:${PORT} asgi:app
+	fi
 else
-    echo "Running (sync) server as Gunicorn"
-    gunicorn -b 0.0.0.0:$PORT wsgi:app --env FLASK_ENV=default --preload
+	echo "Running (sync) server as Gunicorn"
+	gunicorn -b 0.0.0.0:$PORT wsgi:app --env FLASK_ENV=default --preload
 fi
