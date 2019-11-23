b181cbf22161c0da7de4587e65cb5084843a0520
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-11-23 12:03:06 -0300

    Update heroku.sh

--- scripts/heroku.sh
+++ scripts/heroku.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 if [ -n "$SERVER_MODE" ]; then
-	echo ${SERVER_MODE^^}
 	if [ ${SERVER_MODE^^} == *"GUNICORN"* ]; then
 		echo "Running (async) server as Gunicorn"
 		gunicorn -b 0.0.0.0:$PORT asgi:app -w 4 -k uvicorn.workers.UvicornWorker
