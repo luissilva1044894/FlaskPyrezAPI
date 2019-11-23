0418031761cf8b8b423cee0a1e30fb991fa25ea3
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-11-23 15:03:30 -0300

    Update heroku.sh

--- scripts/heroku.sh
+++ scripts/heroku.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-if [ -n "$CMD_LINE" ]; then
-	$CMD_LINE
+#if [ -n "$CMD_LINE" ]; then
+	#$CMD_LINE
+if [ -n "$ASYNC" ]; then
+	echo "Running (async) server as Hypercorn"
+	hypercorn -b 0.0.0.0:${PORT} asgi:app
 else
 	echo "Running server as Gunicorn"
 	gunicorn -b 0.0.0.0:$PORT wsgi:app --env FLASK_ENV=default --preload
