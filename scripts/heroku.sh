9d8f0f6bcb360961e029f0c2ec9e46170d9b45d7
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-11-23 01:43:53 -0300

    Update heroku.sh

--- scripts/heroku.sh
+++ scripts/heroku.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 if [ "$ASYNC" ]; then
-    hypercorn -b 0.0.0.0:${PORT} asgi:app
+    if [ "$UVICORN" ]; then
+    	gunicorn asgi:app -w 4 -k uvicorn.workers.UvicornWorker
+    else
+    	hypercorn -b 0.0.0.0:${PORT} asgi:app
+    fi
 else
     gunicorn -b 0.0.0.0:$PORT wsgi:app --env FLASK_ENV=default --preload
 fi
