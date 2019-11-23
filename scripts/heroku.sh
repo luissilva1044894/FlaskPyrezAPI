b7e88dab6b2b9a8621136c709a218e3b3406beed
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-11-23 12:06:02 -0300

    Update heroku.sh

--- scripts/heroku.sh
+++ scripts/heroku.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-if [ -n "$SERVER_MODE" ]; then
-	if [ ${SERVER_MODE^^} == *"GUNICORN"* ]; then
-		echo "Running (async) server as Gunicorn"
-		gunicorn -b 0.0.0.0:$PORT asgi:app -w 4 -k uvicorn.workers.UvicornWorker
-	elif [ {"$SERVER_MODE"^^} == *"UVICORN"* ]; then
-		echo "Running (async) server as Uvicorn"
-		uvicorn asgi:app --host 0.0.0.0 --port $PORT
-	else
-		echo "Running (async) server as Hypercorn"
-		hypercorn -b 0.0.0.0:${PORT} asgi:app
-	fi
+if [ -n "$CMD_LINE" ]; then
+	$CMD_LINE
 else
-	echo "Running (sync) server as Gunicorn"
+	echo "Running server as Gunicorn"
 	gunicorn -b 0.0.0.0:$PORT wsgi:app --env FLASK_ENV=default --preload
 fi
