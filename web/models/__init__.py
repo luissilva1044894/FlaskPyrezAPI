0129045319b0866b1bbc69912790b011a269fb11
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-11-26 19:56:07 -0300

    Update __init__.py

--- web/models/__init__.py
+++ web/models/__init__.py
@@ -5,11 +5,16 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class CRUD_Mixin(object):
+  @classmethod
+  def create(cls, **kwargs):
+    instance = cls(**kwargs)
+    return instance.save()
   #__bind_key__ = 'database'
   #__abstract__ = True
   id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
   def add(self, resource=None):
+    """Saves the object to the database."""
     '''
     from sqlalchemy.exc import IntegrityError, InternalError, OperationalError, ProgrammingError
     try:
@@ -20,6 +25,10 @@ class CRUD_Mixin(object):
       self.save()
     '''
     db.session.add(resource or self)
+    #db.session.commit()
+    return self
+  def save(self, resource=None):
+    return self.save(resource)
   def append(self, attr, resource):
     getattr(self, attr).append(resource)
   def commit(self):
@@ -27,7 +36,9 @@ class CRUD_Mixin(object):
   def update(self):
   	return db.session.commit()
   def delete(self, resource=None):
+    """Delete the object from the database."""
     db.session.delete(resource or self)
+    return self
   def __repr__(self):
   	return '<{} {}>'.format(self.__class__.__name__, self.to_json())
   def __str__(self):
