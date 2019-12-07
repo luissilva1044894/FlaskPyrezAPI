15d48d8ed986d98fbd9b64d7ff79ff0b5a014ada
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-12-07 12:40:13 -0300

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,30 +1,34 @@
 
 """Flask config class."""
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
 	"""General Configuration."""
-	from utils import random_string, get_env
+	from utils import random_string, get_env, on_heroku
 	from boolify import boolify
 	import os
 
-	ON_HEROKU = boolify(get_env('ON_HEROKU')) or 'heroku' in get_env('PYTHONHOME', '').lower()
+	BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+	TOP_LEVEL_DIR = os.path.abspath(os.curdir)
 
-	PYREZ_DEV_ID=get_env('PYREZ_DEV_ID', -1)
-	PYREZ_AUTH_ID=get_env('PYREZ_AUTH_ID', None)
+	ON_HEROKU = on_heroku()
 
+	PYREZ_DEV_ID, PYREZ_AUTH_ID = get_env('PYREZ_DEV_ID', -1), get_env('PYREZ_AUTH_ID', None)
 
 	DEV_SERVER = get_env('DEV_DISCORD_SERVER', 'https://discord.gg/XkydRPS')
 	GITHUB_REPO = get_env('GITHUB_REPO', 'https://github.com/luissilva1044894/FlaskPyrezAPI')
 
 	# SQLAlchemy
-	SQLALCHEMY_DATABASE_URI = DATABASE_URI = get_env('DATABASE_URL', default='sqlite:///{}'.format(get_env('DATABASE_FILE') or os.path.join(basedir, f'{__name__}.db')))#'sqlite:///:memory:'
+	SQLALCHEMY_DATABASE_URI = DATABASE_URI = get_env('DATABASE_URL') or 'sqlite:///{}'.format(get_env('DATABASE_FILE') or os.path.join(BASE_DIR, f'{__name__}.db'))#'sqlite:///:memory:'
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	#SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 	_binds = get_env('SQLALCHEMY_BINDS', default=None)
 	SQLALCHEMY_BINDS = {}
 	SQLALCHEMY_BINDS.update({'database': SQLALCHEMY_DATABASE_URI})
+	if ON_HEROKU:
+		for _ in os.environ:
+			if _.upper().endswith('_URL'):#if 'DB_URL' in _.upper(): | _.upper().rfind('DB') != -1 and
+				SQLALCHEMY_BINDS.update({_.split('_', 1)[0].lower() : get_env(_)})
+		print(SQLALCHEMY_BINDS)
 	if _binds:
 		#https://docs.sqlalchemy.org/en/13/core/exceptions.html
 		#https://flask-sqlalchemy.palletsprojects.com/en/2.x/binds/
@@ -32,12 +36,6 @@ class Config(object):
 		for _ in _binds.split(','):
 			__ = _.split(':', 1)
 			SQLALCHEMY_BINDS.update({__[0].lower() : __[1] if __[1].rfind('://') != -1 else get_env(__[1])})
-	if ON_HEROKU:
-		for _ in os.environ:
-			if _.upper().endswith('_URL'):#if 'DB_URL' in _.upper(): | _.upper().rfind('DB') != -1 and
-				SQLALCHEMY_BINDS.update({_.split('_', 1)[0].lower() : get_env(_)})
-		print(SQLALCHEMY_BINDS)
-	
 	# SECURITY WARNING: don't run with debug turned on in production!
 	#Default: True if ENV is 'development', or False otherwise.
 	DEBUG = boolify(get_env('DEBUG', default=not ON_HEROKU and os.sys.platform == 'win32' or os.name == 'nt'))
