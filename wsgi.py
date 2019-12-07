3242574521255e5a0d94103920cb7118bd84c836
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-12-07 12:48:06 -0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -9,3 +9,17 @@ if __name__ == '__main__':
   	from web import create_manager
   	create_manager(app).run()
   app.run(debug=app.config['DEBUG'], port=app.config['PORT'], host=app.config['HOST'])
+
+# https://coddyschool.com/upload/Flask_Web_Development_Developing.pdf
+
+'''
+rename to web.wsgi
+#!/usr/bin/python
+import sys
+import logging
+logging.basicConfig(stream=sys.stderr)
+sys.path.insert(0, '/path/to/StreamData/')
+
+from app import app as application
+application.secret_key = 'Add your secret key'
+'''
