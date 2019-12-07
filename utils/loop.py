bffdfdac5f425afbb28244a64047f80abbea995f
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2019-12-07 12:41:01 -0300

    Update loop.py

--- utils/loop.py
+++ utils/loop.py
@@ -1,5 +1,5 @@
 
-def get_event_loop(debug=False):
+def get(force_fresh=False, debug=False):
 	import asyncio
 	import sys
 	if not debug:
@@ -12,10 +12,12 @@ def get_event_loop(debug=False):
 			#uvloop.install()
 			print('>>> Using uvloop')
 			asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-			asyncio.set_event_loop(uvloop.new_event_loop())
-	if sys.platform == 'win32':
-		asyncio.set_event_loop(asyncio.ProactorEventLoop())
-	else:
-		asyncio.set_event_loop(asyncio.new_event_loop())#asyncio.get_event_loop_policy().new_event_loop()
+			#asyncio.set_event_loop(uvloop.new_event_loop())
 	#loop.set_debug(False)
+	if sys.platform == 'win32':
+		if not force_fresh and isinstance(asyncio.get_event_loop(), asyncio.ProactorEventLoop) and not asyncio.get_event_loop().is_closed():
+			return asyncio.get_event_loop()
+		return asyncio.ProactorEventLoop()
+	if force_fresh or asyncio.get_event_loop().is_closed():
+		return asyncio.new_event_loop()#asyncio.get_event_loop_policy().new_event_loop() | asyncio.set_event_loop(asyncio.new_event_loop())
 	return asyncio.get_event_loop()
