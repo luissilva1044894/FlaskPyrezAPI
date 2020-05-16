e857a7d93049b883f4a3279f7f78726a34a82014
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2020-05-16 20:49:05 -0300

    Fix TypeError

--- app/utils/num.py
+++ app/utils/num.py
@@ -10,7 +10,7 @@ from random import (
 def try_int(value, default):
   try:
     return int(value)
-  except ValueError:
+  except (ValueError, TypeError):
     return default
 
 def format_decimal(data, form = ',d'):
