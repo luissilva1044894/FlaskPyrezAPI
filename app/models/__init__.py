55b8a8486675a273da0605f453745d5b5e0b131e
Luís Gustavo <26727089+luissilva1044894@users.noreply.github.com>
2020-06-27 21:05:55 -0300

    Use simple quotes

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -12,9 +12,9 @@ def init_db(app):
   # import all modules here that might define models so that
   # they will be registered properly on the metadata.
   # Otherwise you will have to import them first before calling init_db()
-  app.logger.debug("Initializing Database Tables")
+  app.logger.debug('Initializing Database Tables')
   Base.metadata.create_all(bind=db.engine)
 
 def drop_db(app):
-  app.logger.debug("Dropping Database Tables")
+  app.logger.debug('Dropping Database Tables')
   Base.metadata.drop_all(bind=db.engine)
